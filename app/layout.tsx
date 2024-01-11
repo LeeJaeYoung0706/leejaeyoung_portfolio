992960a8384dcb338dc24b8e7552bc5d563946da
jaesoon3737 <wodud4019@gmail.com>
2024-01-11 16:11:04 +0900

    naver 구글 서치 어드바이저 확인 파일

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type {Metadata} from 'next'
 import StyledComponentsRegistry from "@/styles/StyledComponentRegistry";
 import {ThemeHandlerProvider} from "@/styles/ThemeHandlerContext";
-import {ContainerLayoutStyle, MainViewLayoutStyle} from "@/atom/templates/styles";
 import Header from "@/atom/organisms/header/Header";
 import Footer from "@/atom/organisms/footer/Footer";
-import {NextSeo} from "next-seo";
-
 
 export const metadata: Metadata = {
   title: {
@@ -15,7 +12,15 @@ export const metadata: Metadata = {
   description: '프론트개발자 포트폴리오',
   keywords: ['Next.js', '프론트엔드', '프론트개발자', '포트폴리오'],
   creator: 'Lee Jae Young',
+  openGraph: {
+    type: 'website',
+    siteName: '이재영 개발자 포트폴리오',
+    images: [
+      {url: '/og.svg'}
+    ]
+  }
 }
+
 
 export default function RootLayout({
                                      children,
@@ -24,10 +29,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <NextSeo
-        title={'이재영 개발자 포트폴리오'}
-        description={'Next.js를 활용한 이재영 개발자 포트폴리오입니다.'}
-      />
       <StyledComponentsRegistry>
         <ThemeHandlerProvider>
           <body>
