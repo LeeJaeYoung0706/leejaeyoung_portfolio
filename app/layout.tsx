a0d67b27105ff2029ba79040142822e8808a2b1c
jaesoon3737 <wodud4019@gmail.com>
2024-01-11 15:56:10 +0900

    naver 구글 서치 어드바이저 확인 파일

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import {ThemeHandlerProvider} from "@/styles/ThemeHandlerContext";
 import {ContainerLayoutStyle, MainViewLayoutStyle} from "@/atom/templates/styles";
 import Header from "@/atom/organisms/header/Header";
 import Footer from "@/atom/organisms/footer/Footer";
+import {NextSeo} from "next-seo";
 
 
 export const metadata: Metadata = {
@@ -23,6 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <NextSeo
+        title={'이재영 개발자 포트폴리오'}
+        description={'Next.js를 활용한 이재영 개발자 포트폴리오입니다.'}
+      />
       <StyledComponentsRegistry>
         <ThemeHandlerProvider>
           <body>
