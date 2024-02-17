b03e268c5ca04c737bb1d6ec51f0837d14202d78
jaesoon3737 <wodud4019@gmail.com>
2024-02-17 20:48:02 +0900

    project Array 수정

--- atom/atom/about_me/Id_card/content/AboutMeIdCardContent.tsx
+++ atom/atom/about_me/Id_card/content/AboutMeIdCardContent.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 import styled from "styled-components";
 import {ContentFontSizeMobile, ContentFontSizePC, ContentFontSizeTablet} from "@/styles/commonStyles";
--- atom/organisms/about_me/AboutMe.tsx
+++ atom/organisms/about_me/AboutMe.tsx
@@ -1,4 +1,3 @@
-"use client"
 import React from "react";
 import AboutMeView from "@/atom/organisms/about_me/AboutMeView";
 import {IdCardContentList} from "@/data/aboutme_data";
